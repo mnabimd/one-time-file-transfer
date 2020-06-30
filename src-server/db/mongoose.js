3b3f1b733537344cb5eaaf87b7f4d2862ad39695
mnabimd <mnabimd@gmail.com>
2020-06-30 11:15:45 +0430

    Winston Uninstalled, Mongoose on Connect and Error Msg and Log4js added.

--- src-server/db/mongoose.js
+++ src-server/db/mongoose.js
@@ -1,8 +1,22 @@
 const mongoose = require('mongoose');
+const log4js = require('log4js');
 
-mongoose.connect("mongodb+srv://mohammadnabi:Afghan321@clusterfree.oikmt.mongodb.net/one-time-file-transfer-api?retryWrites=true&w=majority", {
+log4js.configure({
+  appenders: {cheese: { type: 'file', filename: './src-server/logs/log4js.log'} },
+  categories: { default: { appenders: ['cheese'], level: 'error'}}
+});
+
+const logger = log4js.getLogger('cheese');
+
+mongoose.connect('mongodb+srv://mohammad:Afghan321@cluster0.d2iyo.mongodb.net/transfer-api?retryWrites=true&w=majority', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true
-})
+}).then(() => {
+    console.log('Database Connected!')
+}).catch(e => {
+    console.log(e);
+    logger.error(e);
+});
+
