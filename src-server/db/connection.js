8b63d0e6ca493b8c56ce3732bf1a101655f7f7a7
mnabimd <mnabimd@gmail.com>
2020-07-10 21:30:18 +0430

    DB>connection.js values changed into env vars.

--- src-server/db/connection.js
+++ src-server/db/connection.js
@@ -1,6 +1,22 @@
 const Sequelize = require('sequelize');
+const log4js = require('log4js');
 
-const sequelize =  new Sequelize('transfer_api', 'root', '', { host: '127.0.0.1', dialect: 'mysql', operatorsAliases: 0, logging: false});
+log4js.configure({
+  appenders: {cheese: { type: 'file', filename: './src-server/logs/log4js.log'} },
+  categories: { default: { appenders: ['cheese'], level: 'error'}}
+});
+
+const logger = log4js.getLogger('cheese');
+
+const sequelize =  new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASSWORD, { host: process.env.DB_HOST, dialect: 'mysql', operatorsAliases: 0, logging: false});
+
+const connectStatus = sequelize.authenticate().then(function(){
+    console.log('Connected Successfully!');
+    logger.error('Connected Successfully!');
+}).catch(function(e) {
+    console.log({error: 'Error', e});
+    logger.error(e);
+});
 
 module.exports = sequelize;
 global.sequelize = sequelize;
