b78de3550a132265b473d9f342cc8bae25601401
mnabimd <mnabimd@gmail.com>
2020-07-10 17:54:53 +0430

    Config.json changed to config.js

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+      username: 'root',
+      password: null,
+      database: 'transfer_api',
+      host: "127.0.0.1",
+      dialect: 'mysql'
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'transfer_api',
+    host: "127.0.0.1",
+    dialect: 'mysql'
+  },
+  production: {
+    username: process.env.DB_USERNAMW,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: "mysql"
+  }
+}
