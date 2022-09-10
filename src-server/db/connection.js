03a6758c1e5d44009eb87c777ea11f200bee88b1
Mohammad Nabi <mnabimd@gmail.com>
2022-09-10 22:55:26 +0430

    the unused (scss) folder has been deleted

--- src-server/db/connection.js
+++ src-server/db/connection.js
@@ -1,19 +1,27 @@
 const Sequelize = require('sequelize');
 
 // 1. DB_NAME 2. DB-USERNAME 3. DB_PASSWORD 4. DB_HOST
-const sequelize =  new Sequelize('mnabimd_transfer_api', 'mnabimd_mnabimd', 'Afghan321', { 
-    host: 'mnabi.heliohost.org', 
-    dialect: 'mysql', 
-    operatorsAliases: 0, 
-    logging: false, 
-    dialectModule: require('mysql2')
-});
+const sequelize = new Sequelize(
+   process.env.DB_NAME,
+   process.env.DB_USERNAME,
+   process.env.DB_PASSWORD,
+   {
+      host: process.env.DB_HOST,
+      dialect: 'mysql',
+      operatorsAliases: 0,
+      logging: false,
+      dialectModule: require('mysql2')
+   }
+);
 
-const connectStatus = sequelize.authenticate().then(function(){
-    console.log('Connected Successfully!');
-}).catch(function(e) {
-    console.log({error: 'Error', e});
-});
+const connectStatus = sequelize
+   .authenticate()
+   .then(function () {
+      console.log('Connected Successfully!');
+   })
+   .catch(function (e) {
+      console.log({ error: 'Error', e });
+   });
 
-module.exports = {sequelize};
+module.exports = { sequelize };
 global.sequelize = sequelize;
